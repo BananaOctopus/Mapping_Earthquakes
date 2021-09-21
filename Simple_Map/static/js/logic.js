de93261519346a48f3467571ec4506a36e9e12af
BananaOctopus <84158312+BananaOctopus@users.noreply.github.com>
2021-09-21 16:38:15 -0400

    Updating files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -13,16 +13,16 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //   ],
 //   zoom: 4
 // });
-// Add a tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-});
- streets.addTo(map);
+// // Add a tile layer
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// });
+//  streets.addTo(map);
 // //This method is useful when we need to add multiple tile layers, 
 // //or a background image of our map(s), which we will do later in this module.
 // // We create the tile layer that will be the background of our map.
@@ -31,5 +31,11 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 //     maxZoom: 18,
 //     accessToken: API_KEY
 // });
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 // // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
+streets.addTo(map);
